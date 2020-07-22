192875b73621a1222323eb739786bd4a67563843
Stanley Wintergreen <kepocnhh@gmail.com>
2020-07-22 14:43:31 +0300

    wip | pipeline verify test coverage

--- buildSrc/src/main/resources/script/testing_script.sh
+++ buildSrc/src/main/resources/script/testing_script.sh
@@ -11,4 +11,15 @@ else
   echo "task $task error"
 fi
 
+status=0
+task="lib:verifyTestCoverage"
+echo "run task $task"
+gradle -q $task || status=$?
+if test $status -eq 0; then
+  echo "task $task success"
+else
+  if test $SCRIPT_STATUS -eq 0; then SCRIPT_STATUS=1; fi
+  echo "task $task error"
+fi
+
 exit $SCRIPT_STATUS
