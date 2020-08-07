3078625b359b152cc76b0860e4d970c29ad4634c
Stanley Wintergreen <kepocnhh@gmail.com>
2020-08-07 21:14:25 +0300

    wip | notification message fix

--- buildSrc/src/main/resources/bash/workflow/after.sh
+++ buildSrc/src/main/resources/bash/workflow/after.sh
@@ -5,7 +5,7 @@ REPO_NAME=$GITHUB_REPO
 REPO_URL=https://github.com/$REPO_OWNER/$REPO_NAME
 
 TOP="GitHub build [#$GITHUB_RUN_NUMBER]($REPO_URL/actions/runs/$GITHUB_RUN_ID)"
-MID=""
+BOT=""
 
 if test $IS_LIGHTWEIGHT_BUILD_INTERNAL == $TRUE; then
     TOP="$TOP skipped"
@@ -23,10 +23,10 @@ else
             body=$(<file); rm file
             merged=$(echo $body | jq -r .merged)
             if test $merged == true; then
-                MID="
+                BOT="
+
 pull request [#$PR_NUMBER](https://github.com/$GITHUB_OWNER/$GITHUB_REPO/pull/$PR_NUMBER) \
-merged by [$GIT_WORKER_NAME](https://github.com/$GITHUB_WORKER_LOGIN)
-"
+merged by [$GIT_WORKER_NAME](https://github.com/$GITHUB_WORKER_LOGIN)"
             else
                 echo "Pull request #$PR_NUMBER not merged"
             fi
@@ -46,36 +46,36 @@ if test -z $GITHUB_AUTHOR_LOGIN; then
                         echo "No responsible user!"
                         return 2
                     else
-                        user="committer $GIT_COMMITTER_EMAIL"
+                        user="$GIT_COMMITTER_EMAIL"
                     fi
                 else
-                    user="committer $GIT_COMMITTER_NAME"
+                    user="$GIT_COMMITTER_NAME"
                 fi
             else
                 if test -z "$GIT_COMMITTER_NAME"; then
-                    user="committer [$GITHUB_COMMITTER_LOGIN](https://github.com/$GITHUB_COMMITTER_LOGIN)"
+                    user="[$GITHUB_COMMITTER_LOGIN](https://github.com/$GITHUB_COMMITTER_LOGIN)"
                 else
-                    user="committer [$GIT_COMMITTER_NAME](https://github.com/$GITHUB_COMMITTER_LOGIN)"
+                    user="[$GIT_COMMITTER_NAME](https://github.com/$GITHUB_COMMITTER_LOGIN)"
                 fi
             fi
         else
-            user="author $GIT_AUTHOR_EMAIL"
+            user="$GIT_AUTHOR_EMAIL"
         fi
     else
-        user="author $GIT_AUTHOR_NAME"
+        user="$GIT_AUTHOR_NAME"
     fi
 else
     if test -z "$GIT_AUTHOR_NAME"; then
-        user="author [$GITHUB_AUTHOR_LOGIN](https://github.com/$GITHUB_AUTHOR_LOGIN)"
+        user="[$GITHUB_AUTHOR_LOGIN](https://github.com/$GITHUB_AUTHOR_LOGIN)"
     else
-        user="author [$GIT_AUTHOR_NAME](https://github.com/$GITHUB_AUTHOR_LOGIN)"
+        user="[$GIT_AUTHOR_NAME](https://github.com/$GITHUB_AUTHOR_LOGIN)"
     fi
 fi
 
 MESSAGE="$TOP
 
-[$REPO_OWNER](https://github.com/$REPO_OWNER)/[$REPO_NAME]($REPO_URL)/[${GIT_COMMIT_SHA::7}]($REPO_URL/commit/$GIT_COMMIT_SHA)
-$MID
-$user"
+[$REPO_OWNER](https://github.com/$REPO_OWNER) / [$REPO_NAME]($REPO_URL)
+
+[${GIT_COMMIT_SHA::7}]($REPO_URL/commit/$GIT_COMMIT_SHA) by $user$BOT"
 
 bash $WORKFLOW/telegram_send_message.sh "$MESSAGE"
